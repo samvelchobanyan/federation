50c89f8f035a78f677092a24990addfb79c75d40
samvel <samvel.chobanyan@gmail.com>
2024-07-05 19:27:23 +0400

    asd

--- lib/providers/persons_provider.dart
+++ lib/providers/persons_provider.dart
@@ -2,6 +2,6 @@ import 'package:federation/repositories/persons_repo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final personProvider = FutureProvider((ref) async{
-  // final per = PersonRepo().getPrsons(limit: 5,ofset: 0);
-  return PersonRepo().getPrsons(limit: 5,ofset: 0);
+  final personsList = PersonRepo().getPrsons(limit: 5,ofset: 0);
+  return personsList;
 });
